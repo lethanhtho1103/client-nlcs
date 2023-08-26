591d8fa4af0510498099177a30d0a777a7a46599
lethanhtho1103 <lethanhtho1953@gamil.com>
2023-08-27 00:43:23 +0700

    Dựng base client-nlcs

---
+++ src/components/FormLogin/index.js
@@ -0,0 +1,5 @@
+function FormLogin() {
+    return <h2>Form Login</h2>;
+}
+
+export default FormLogin;
---
+++ src/pages/Login/index.js
@@ -0,0 +1,6 @@
+import FormLogin from '~/components/FormLogin';
+function Login() {
+    return <FormLogin />;
+}
+
+export default Login;
